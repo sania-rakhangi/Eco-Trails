b0311db7ce4260389f8f83831dd8f1f8c55ac4fc
momo <cmomo615@gmail.com>
2024-02-24 18:27:26 +0530

    list

--- src/components/List/List.jsx
+++ src/components/List/List.jsx
@@ -1,8 +1,37 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { CircularProgress, Grid, Typography, InputLabel, MenuItem, FormControl, Select } from '@material-ui/core';
+
+import useStyles from './styles';
 
 const List = () => {
+
+    const classes = useStyles();
+    const [type, setType] = useState('restaurants');
+    const [rating, setRating] = useState('restaurants');
+
     return (
-        <h1>List</h1>
+      <div className= {classes.constainer}>
+        <Typography variant="h4">Restaurants, Hotels and attractions around you</Typography>
+        <FormControl className = {classes.FormControl}>
+            <InputLabel>Type</InputLabel>
+            <Select value={type} onChange={(e)=> setType(e.target.value)}>
+                <MenuItem value = "restaurants">restaurants</MenuItem>
+                <MenuItem value = "hotes">hotels</MenuItem>
+                <MenuItem value = "attractions">attractions</MenuItem>
+            </Select>
+        </FormControl>
+
+        <FormControl className = {classes.FormControl}>
+            <InputLabel>Rating</InputLabel>
+            <Select value={rating} onChange={(e)=> setRating(e.target.value)}>
+                <MenuItem value = {0}>All</MenuItem>
+                <MenuItem value = {3}>Above 3.0</MenuItem>
+                <MenuItem value = {4}>Above 4.0</MenuItem>
+                <MenuItem value = {4.5}>Above 4.5</MenuItem>
+            </Select>
+        </FormControl>
+      </div>
+
     );
 }
 

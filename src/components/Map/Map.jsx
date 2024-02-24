fa64bb4469368b0f96ebe9e8c281f57c8aec63d5
VaishnaviL2005 <144267388+VaishnaviL2005@users.noreply.github.com>
2024-02-24 17:32:05 +0530

    Add files via upload
    Updated map component

--- src/components/Map/Map.jsx
+++ src/components/Map/Map.jsx
@@ -1,9 +1,32 @@
 import React from 'react';
+import GoogleMapReact from 'google-map-react';
+import { Paper, Typography, useMediaQuery } from '@material-ui/core';
+
+import LocationOnOutlinedIcon from '@material-ui/icons/LocationOnOutlined';
+// import Rating from '@material-ui/lab';
+
+import useStyles from './styles.js';
 
 const Map = () => {
+    
+    const classes = useStyles();
+    const isMobile = useMediaQuery('(min-width:600px)');
+    const cordinates={lat :0, lng:0};
     return (
-        <h1>Map</h1>
+      <div className={classes.mapContainer}>
+        <GoogleMapReact
+          bootstrapURLKeys={{key:'AIzaSyCTNQwMD4VoO0jZovq2CvrlKnJkEMdBJu0'}}
+          defaultCenter={cordinates}
+          center={cordinates}
+          defaultZoom={14}
+          margin={[50, 50, 50, 50]}
+          options={''}
+          onChange={''}
+          onChildClick={''}
+        >
+        </GoogleMapReact>
+      </div>
     );
-}
-
-export default Map;
+  };
+  
+  export default Map;

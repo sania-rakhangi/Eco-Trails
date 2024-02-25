98430f48fbdc12d65a539fab05e6d3a48866fa44
sania-rakhangi <147305583+sania-rakhangi@users.noreply.github.com>
2024-02-25 10:43:44 +0530

    map changed

--- src/components/Map/Map.jsx
+++ src/components/Map/Map.jsx
@@ -3,25 +3,28 @@ import GoogleMapReact from 'google-map-react';
 import { Paper, Typography, useMediaQuery } from '@material-ui/core';
 
 import LocationOnOutlinedIcon from '@material-ui/icons/LocationOnOutlined';
-// import Rating from '@material-ui/lab';
+import Rating from '@material-ui/lab';
 
 import useStyles from './styles.js';
 
-const Map = () => {
+const Map = ({ setCoordinates, setBounds, coordinates}) => {
     
     const classes = useStyles();
     const isMobile = useMediaQuery('(min-width:600px)');
-    const cordinates={lat :0, lng:0};
+
     return (
       <div className={classes.mapContainer}>
         <GoogleMapReact
           bootstrapURLKeys={{key:'AIzaSyCTNQwMD4VoO0jZovq2CvrlKnJkEMdBJu0'}}
-          defaultCenter={cordinates}
-          center={cordinates}
+          defaultCenter={coordinates}
+          center={coordinates}
           defaultZoom={14}
           margin={[50, 50, 50, 50]}
           options={''}
-          onChange={''}
+          onChange={(e) => {
+            setCoordinates({ lat: e.center.lat, lng: e.center.lng });
+            setBounds({ ne: e.marginBounds.ne, sw: e.marginBounds.sw});
+          }}
           onChildClick={''}
         >
         </GoogleMapReact>
